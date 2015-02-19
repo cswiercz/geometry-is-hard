229f24e3f3c294511efc200c931533a6c98b5b1e
danshapero <shapero.daniel@gmail.com>
2015-02-19 12:38:43 -0800

    Changed int to size_t

--- primitives.hpp
+++ primitives.hpp
@@ -2,19 +2,19 @@
 #ifndef PRIMITIVES_HPP
 #define PRIMITIVES_HPP
 
-template <int dim,
+template <size_t dim,
           class T,
           class Real = double>
 class PointBase
 {
-  Real operator[](const unsigned int &i) const
+  Real operator[](const size_t& i) const
   {
     return static_cast<T*>(this)[i];
   }
 };
 
 
-template <int dim,
+template <size_t dim,
 	  class Real = double>
 class Point : PointBase<dim, Point<dim> >
 {
@@ -22,13 +22,9 @@ protected:
   Real coords[dim];
 
 public:
-  Point() {
-    for (unsigned int i = 0; i < dim; i++) {
-      coords[i] = 0.0;
-    }
-  }
+  Point() : coords() { }
 
-  Real operator[](const unsigned int &i) const
+  Real operator[](const size_t& i) const
   {
     return coords[i];
   }
